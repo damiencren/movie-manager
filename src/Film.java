5304fd5636027edc96393cd48cef7c971eb0c3a1
Damien Cren <68796664+damiencren@users.noreply.github.com>
2022-12-13 16:30:34 +0100

    Update Film.java

---
+++ src/Film.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class Film {
+    private String title;
+    private int year;
+    private String genre;
+    private int duration;
+    private String country;
+    private String language;
+    private String director;
+    private String scriptwriter;
+
+
+}
