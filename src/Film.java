bbdfaad1170f89f97f91c5e66a7dd8261bbc4f96
Damien Cren <68796664+damiencren@users.noreply.github.com>
2022-12-13 17:02:10 +0100

    Update Film.java

--- src/Film.java
+++ src/Film.java
@@ -30,4 +30,58 @@ public class Film {
         voteCount=vc;
         voteAverage=va;
     }
+
+	static public Comparator<Film> compareTitle = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.title.compareToIgnoreCase(f2.title);
+	    }
+	};
+
+    static public Comparator<Film> compareYear = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.year-f2.year;
+	    }
+	};
+
+    static public Comparator<Film> compareGenre = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.genre.compareToIgnoreCase(f2.genre);
+	    }
+	};
+
+    static public Comparator<Film> compareDuration = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.duration-f2.duration;
+	    }
+	};
+
+    static public Comparator<Film> compareCountry = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.country.compareToIgnoreCase(f2.country);
+	    }
+	};
+
+    static public Comparator<Film> compareDirector = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.director.compareToIgnoreCase(f2.director);
+	    }
+	};
+
+    static public Comparator<Film> compareVoteCount = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.voteCount-f2.voteCount;
+	    }
+	};
+
+    static public Comparator<Film> compareVoteAverage = new Comparator <Film>() {
+	    public int compare(Film f1, Film f2) {
+	        return f1.voteAverage-f2.voteAverage;
+	    }
+	};
+    
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
