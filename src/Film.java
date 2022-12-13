61ced9d9a3553c6008bf80d2bee1d01b94e0ef93
Damien Cren <68796664+damiencren@users.noreply.github.com>
2022-12-13 16:48:06 +0100

    Update Film.java

--- src/Film.java
+++ src/Film.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Film {
@@ -9,6 +10,24 @@ public class Film {
     private String language;
     private String director;
     private String scriptwriter;
+    private ArrayList<String> actorsList;
+    private String description;
+    private int voteCount;
+    private int voteAverage;
 
-
+    Film(String t, int y, String g, int du, String c, String l, String di, String s, ArrayList<String> a, String d, int vc, int va)
+    {
+        title=t;
+        year=y;
+        genre=g;
+        duration=du;
+        country=c;
+        language=l;
+        director=di;
+        scriptwriter=s;
+        actorsList=a;
+        description=d;
+        voteCount=vc;
+        voteAverage=va;
+    }
 }
